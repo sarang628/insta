fe38180ce5b1a328a1cf7c69ed4364ef3499f008
Sarang Yang <sarangyang@Sarangui-MacBookPro.local>
2020-04-28 16:21:28 +0900

    update

--- app/src/main/java/com/sarang/instagramlogin/SignUpFragment.java
+++ app/src/main/java/com/sarang/instagramlogin/SignUpFragment.java
@@ -9,6 +9,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.google.android.material.tabs.TabLayout;
 
@@ -35,12 +36,18 @@ public class SignUpFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
 
         view.findViewById(R.id.cl_phone).setSelected(true);
+        view.findViewById(R.id.cl_input_phone).setVisibility(View.VISIBLE);
+        view.findViewById(R.id.tv_sign_up_comment).setVisibility(View.VISIBLE);
+        view.findViewById(R.id.et_email).setVisibility(View.GONE);
 
         view.findViewById(R.id.cl_email).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 view.findViewById(R.id.cl_email).setSelected(true);
                 view.findViewById(R.id.cl_phone).setSelected(false);
+                view.findViewById(R.id.cl_input_phone).setVisibility(View.GONE);
+                view.findViewById(R.id.tv_sign_up_comment).setVisibility(View.GONE);
+                view.findViewById(R.id.et_email).setVisibility(View.VISIBLE);
             }
         });
 
@@ -49,6 +56,16 @@ public class SignUpFragment extends Fragment {
             public void onClick(View v) {
                 view.findViewById(R.id.cl_email).setSelected(false);
                 view.findViewById(R.id.cl_phone).setSelected(true);
+                view.findViewById(R.id.cl_input_phone).setVisibility(View.VISIBLE);
+                view.findViewById(R.id.tv_sign_up_comment).setVisibility(View.VISIBLE);
+                view.findViewById(R.id.et_email).setVisibility(View.GONE);
+            }
+        });
+
+        view.findViewById(R.id.btn_next).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getContext(), "show", Toast.LENGTH_SHORT).show();
             }
         });
     }
